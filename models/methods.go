100299d9f59bb91af85a7e5b88efe75617bbbf94
= <=>
2025-06-30 23:05:18 +0300

    handling appending values instead of rewriting

--- models/methods.go
+++ models/methods.go
@@ -1,13 +1,17 @@
 package models
 
 import (
+	"errors"
 	"fmt"
+	"ringo/errs"
 	"time"
 )
 
+var ()
+
 func (r *RinGoObject) Store(key string, value interface{}, duration time.Duration) error {
-	if key == "" || value == nil {
-		return fmt.Errorf("the key or value is nil")
+	if key == "" {
+		return fmt.Errorf("key: %s, %w", key, errs.ErrNilKey)
 	}
 	var exp time.Time
 	if duration != 0 {
@@ -17,27 +21,31 @@ func (r *RinGoObject) Store(key string, value interface{}, duration time.Duratio
 	case string:
 		r.Values[key] = GlobalObject{Value: v, ExpirationTime: exp}
 	case []string:
-		r.Values[key] = GlobalObject{Value: v, ExpirationTime: exp}
+		if err := r.handleValues(v, key, exp); err != nil {
+			return err
+		}
 	case map[string]string:
-		r.Values[key] = GlobalObject{Value: v, ExpirationTime: exp}
+		if err := r.handleValues(v, key, exp); err != nil {
+			return err
+		}
 	default:
-		return fmt.Errorf("failed to define the datatype of the value")
+		return errs.ErrDatatype
 	}
 	return nil
 }
 
 func (r *RinGoObject) Get(key string) (interface{}, error) {
 	if key == "" {
-		return nil, fmt.Errorf("key is nil")
+		return nil, fmt.Errorf("key: %s, %w", key, errs.ErrNilKey)
 	}
 	val, ok := r.Values[key]
 	if !ok {
-		return nil, fmt.Errorf("value wasn't found")
+		return nil, fmt.Errorf("key: %s, %w", key, errs.ErrNotFound)
 	}
 
 	if !val.ExpirationTime.IsZero() && time.Now().After(val.ExpirationTime) {
 		delete(r.Values, key)
-		return nil, fmt.Errorf("value was deleted")
+		return nil, fmt.Errorf("key: %s, %w", key, errs.ErrKeyDeleted)
 	}
 
 	switch v := val.Value.(type) {
@@ -48,16 +56,68 @@ func (r *RinGoObject) Get(key string) (interface{}, error) {
 	case map[string]string:
 		return v, nil
 	default:
-		return nil, fmt.Errorf("failed to define the datatype of the value")
+		return nil, errors.New("failed to define the datatype of the value")
 	}
 }
 
 func (r *RinGoObject) Delete(key string) error {
 	if key == "" {
-		return fmt.Errorf("key is nil")
+		return fmt.Errorf("key: %s, %w", key, errs.ErrNilKey)
 	}
 	delete(r.Values, key)
 	return nil
+}
+
+func (r *RinGoObject) getExpirationDate(key string) (time.Time, error) {
+	if key == "" {
+		return time.Time{}, fmt.Errorf("key: %s, %w", key, errs.ErrNilKey)
+	}
+	val, ok := r.Values[key]
+	if !ok {
+		return time.Time{}, fmt.Errorf("key: %s, %w", key, errs.ErrNotFound)
+	}
+	return val.ExpirationTime, nil
+}
+
+func (r *RinGoObject) handleValues(newVal interface{}, key string, exp time.Time) error {
+	val, err := r.Get(key)
+	if err != nil {
+		r.Values[key] = GlobalObject{Value: newVal, ExpirationTime: exp}
+		return nil
+	}
+	oldExp, errExp := r.getExpirationDate(key)
+	if errExp != nil {
+		oldExp = exp
+	}
+	merged, err := mergeFunc(val, newVal)
+	if err != nil {
+		return err
+	}
+	r.Values[key] = GlobalObject{Value: merged, ExpirationTime: oldExp}
+	return nil
+}
+
+func mergeFunc(val interface{}, newVal interface{}) (interface{}, error) {
+	switch v := val.(type) {
+	case []string:
+		v2, ok := newVal.([]string)
+		if !ok {
+			return nil, errs.ErrDatatype
+		}
+		v = append(v, v2...)
+		return v, nil
+	case map[string]string:
+		v2, ok := newVal.(map[string]string)
+		if !ok {
+			return nil, errs.ErrDatatype
+		}
+		for k, value := range v2 {
+			v[k] = value
+		}
+		return v, nil
+	default:
+		return nil, errs.ErrDatatype
+	}
 }
 
 func NewRinGoObject() *RinGoObject {
