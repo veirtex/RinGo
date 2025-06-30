088115a3ab59b46c9166fe9cd9e26918ddf408fc
= <=>
2025-06-30 23:04:52 +0300

    storing errors

---
+++ errs/errtypes.go
@@ -0,0 +1,11 @@
+package errs
+
+import "errors"
+
+var (
+	ErrArgs       = errors.New("args are not enough")
+	ErrNotFound   = errors.New("key doesn't exist")
+	ErrKeyDeleted = errors.New("key deleted")
+	ErrNilKey     = errors.New("key is nil")
+	ErrDatatype   = errors.New("datatype is unknown")
+)
