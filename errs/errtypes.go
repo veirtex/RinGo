5ee40564d24a9925226312315922981103dc777c
= <=>
2025-06-30 23:52:18 +0300

    new error

--- errs/errtypes.go
+++ errs/errtypes.go
@@ -3,9 +3,10 @@ package errs
 import "errors"
 
 var (
-	ErrArgs       = errors.New("args are not enough")
-	ErrNotFound   = errors.New("key doesn't exist")
-	ErrKeyDeleted = errors.New("key deleted")
-	ErrNilKey     = errors.New("key is nil")
-	ErrDatatype   = errors.New("datatype is unknown")
+	ErrArgs           = errors.New("args are not enough")
+	ErrNotFound       = errors.New("key doesn't exist")
+	ErrKeyDeleted     = errors.New("key deleted")
+	ErrNilKey         = errors.New("key is nil")
+	ErrDatatype       = errors.New("datatype is unknown")
+	ErrUnknownCommand = errors.New("unknown command")
 )
