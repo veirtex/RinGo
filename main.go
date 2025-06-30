ecd68a9b676f7e0c5dbd30e4d8c6f53eb2fa37f2
= <=>
2025-06-30 23:51:30 +0300

    removed panic and added command for get

--- main.go
+++ main.go
@@ -5,35 +5,48 @@ import (
 	"log"
 	"os"
 	"ringo/errs"
+	"ringo/handlers"
 	"ringo/models"
 )
 
 func main() {
 	args := os.Args
-	if len(args) < 4 {
-		log.Panic(errs.ErrArgs)
+	if len(args) < 3 {
+		log.Printf("Error: %s", errs.ErrArgs)
+		return
 	}
-	handlers := map[string]Handler{
-		"set":    SetHandler{},
-		"sset":   SSetHandler{},
-		"hset":   HSetHandler{},
-		"delete": DeleteHandler{},
+	handlrs := map[string]handlers.Handler{
+		"set":    handlers.SetHandler{},
+		"sset":   handlers.SSetHandler{},
+		"hset":   handlers.HSetHandler{},
+		"delete": handlers.DeleteHandler{},
 	}
 	var command string
 	if len(args) > 1 {
 		command = args[1]
 	} else {
+		log.Printf("Error: %s", errs.ErrUnknownCommand)
 		return
 	}
 
 	var r models.RinGoObject
 	r.Values = make(map[string]models.GlobalObject)
-	if handler, ok := handlers[command]; ok {
+	if handler, ok := handlrs[command]; ok {
 		err := handler.Handle(args, &r)
 		if err != nil {
-			fmt.Println("Error:", err)
+			log.Printf("Error: %s", err)
+		} else {
+			fmt.Println("Done")
 		}
 	} else {
-		fmt.Println("Unknown command:", command)
+		if command == "get" {
+			if val, err := handlers.GetHandle(args, &r); err != nil {
+				log.Printf("Error: %s", err)
+			} else {
+				fmt.Println(val)
+			}
+		} else {
+			log.Printf("Error: %s", errs.ErrUnknownCommand)
+		}
 	}
 }
