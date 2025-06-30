b194e80df2029e2d086c30f74c97f3a3566054d2
= <=>
2025-06-30 23:04:39 +0300

    Seperated the store handler to 3 handlers

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"ringo/errs"
 	"ringo/models"
 	"strconv"
 	"strings"
@@ -12,7 +13,11 @@ type Handler interface {
 	Handle(args []string, r *models.RinGoObject) error
 }
 
-type StoreHandler struct{}
+type SetHandler struct{}
+
+type SSetHandler struct{}
+
+type HSetHandler struct{}
 
 type GetHandler struct{}
 
@@ -27,66 +32,106 @@ func processTime(sndStr string) (time.Duration, error) {
 	}
 }
 
-func (h StoreHandler) Handle(args []string, r *models.RinGoObject) error {
+func (h SetHandler) Handle(args []string, r *models.RinGoObject) error {
 	if len(args) < 4 {
-		return fmt.Errorf(`args are not enough try "command" -h `)
+		return errs.ErrArgs
 	}
-	command := args[1]
-	switch strings.ToLower(command) {
-	case "set":
-		key := args[2]
-		value := args[3]
-		var exp time.Duration
-		if len(args) > 6 {
-			if strings.ToLower(args[4]) == "exp" {
-				if dur, err := processTime(args[5]); err != nil {
-					return err
-				} else {
-					exp = dur
-				}
+	key := args[2]
+	value := args[3]
+	var exp time.Duration
+	if len(args) > 5 {
+		if strings.ToLower(args[4]) == "exp" {
+			if dur, err := processTime(args[5]); err != nil {
+				return err
+			} else {
+				exp = dur
 			}
 		}
-		if err := r.Store(key, value, exp); err != nil {
-			return err
-		}
-		return nil
-	case "sset":
-		key := args[2]
-		values := []string{}
-		var exp time.Duration
-		for ind, arg := range args[3:] {
-			if strings.ToLower(arg) == "exp" {
-				if len(args[3:]) >= ind+1 {
-					if dur, err := processTime(args[3:][ind+1]); err != nil {
-						return err
-					} else {
-						exp = dur
-					}
-				}
-				break
-			}
-			values = append(values, arg)
-		}
-		if err := r.Store(key, values, exp); err != nil {
-			return err
-		} else {
-			return nil
-		}
-	case "hset":
-		// TODO
-		return nil
-	default:
-		// TODO
-		return nil
 	}
-}
-
-func (h GetHandler) Handle(args []string, r *models.RinGoObject) error {
-	// TODO
+	if err := r.Store(key, value, exp); err != nil {
+		return err
+	}
 	return nil
 }
 
+func (h SSetHandler) Handle(args []string, r *models.RinGoObject) error {
+	key := args[2]
+	values := []string{}
+	var exp time.Duration
+	for i := 3; i < len(args); i++ {
+		if strings.ToLower(args[i]) == "exp" {
+			if i+1 < len(args) {
+				dur, err := processTime(args[i+1])
+				if err != nil {
+					return err
+				}
+				exp = dur
+			}
+			break
+		}
+		values = append(values, args[i])
+	}
+	if err := r.Store(key, values, exp); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (h HSetHandler) Handle(args []string, r *models.RinGoObject) error {
+	key := args[2]
+	values := make(map[string]string)
+	var exp time.Duration
+	for i := 3; i < len(args); i += 2 {
+		if strings.ToLower(args[i]) == "exp" {
+			if i+1 < len(args) {
+				dur, err := processTime(args[i+1])
+				if err != nil {
+					return err
+				}
+				exp = dur
+			}
+			break
+		}
+		if i+1 >= len(args) {
+			return fmt.Errorf("expected value after key '%s'", args[i])
+		}
+		hsetKey := args[i]
+		hsetVal := args[i+1]
+		values[hsetKey] = hsetVal
+	}
+	if err := r.Store(key, values, exp); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (h GetHandler) Handle(args []string, r *models.RinGoObject) (interface{}, error) {
+	if len(args) < 3 {
+		return nil, errs.ErrArgs
+	}
+	key := args[2]
+	if key == "" {
+		return nil, errs.ErrNilKey
+	}
+	if val, err := r.Get(key); err != nil {
+		return nil, err
+	} else {
+		return val, nil
+	}
+}
+
 func (h DeleteHandler) Handle(args []string, r *models.RinGoObject) error {
-	// TODO
+	if len(args) < 3 {
+		return errs.ErrArgs
+	}
+	key := args[2]
+	if key == "" {
+		return errs.ErrNilKey
+	}
+	if err := r.Delete(key); err != nil {
+		return err
+	}
 	return nil
 }
