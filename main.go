18adf25fefc7d5054147299ff0d0c540e6893013
= <=>
2025-07-01 22:16:13 +0300

    removed cli parsing and used user input

--- main.go
+++ main.go
@@ -1,52 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"ringo/errs"
 	"ringo/handlers"
 	"ringo/models"
+	"strings"
 )
 
 func main() {
-	args := os.Args
-	if len(args) < 3 {
-		log.Printf("Error: %s", errs.ErrArgs)
-		return
-	}
+	isRunning := true
 	handlrs := map[string]handlers.Handler{
 		"set":    handlers.SetHandler{},
 		"sset":   handlers.SSetHandler{},
 		"hset":   handlers.HSetHandler{},
+		"get":    handlers.GetHandler{},
 		"delete": handlers.DeleteHandler{},
 	}
 	var command string
-	if len(args) > 1 {
-		command = args[1]
-	} else {
-		log.Printf("Error: %s", errs.ErrUnknownCommand)
-		return
-	}
-
 	var r models.RinGoObject
 	r.Values = make(map[string]models.GlobalObject)
-	if handler, ok := handlrs[command]; ok {
-		err := handler.Handle(args, &r)
-		if err != nil {
-			log.Printf("Error: %s", err)
-		} else {
-			fmt.Println("Done")
+	for isRunning {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		command = scanner.Text()
+		args := handleCommand(command)
+		if args == nil {
+			log.Printf("%s", errs.ErrArgs)
+			continue
 		}
-	} else {
-		if command == "get" {
-			if val, err := handlers.GetHandle(args, &r); err != nil {
-				log.Printf("Error: %s", err)
+		if args[0] == "exit" {
+			isRunning = false
+			break
+		}
+		if handler, ok := handlrs[args[0]]; ok {
+			res, err := handler.Handle(args, &r)
+			if err != nil {
+				log.Printf("%s", err)
 			} else {
-				fmt.Println(val)
+				printing(res)
 			}
 		} else {
-			log.Printf("Error: %s", errs.ErrUnknownCommand)
+			log.Printf("%s", errs.ErrUnknownCommand)
 		}
+	}
+}
+
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func handleCommand(command string) []string {
+	fields := strings.Fields(command)
+	return fields
+}
+
+func printing(val interface{}) {
+	switch v := val.(type) {
+	case bool:
+		fmt.Printf("%d\n", boolToInt(v))
+	case string:
+		fmt.Println(v)
+	case []string:
+		fmt.Println(strings.Join(v, ", "))
+	case map[string]string:
+		for k, value := range v {
+			fmt.Printf("%s: %s ", k, value)
+		}
+		fmt.Println()
+	default:
+		log.Println(errs.ErrDatatype)
 	}
 }
